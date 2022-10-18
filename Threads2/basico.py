fd18ae2a5e16acaeb2ddd1183bbe7fb23f6e10ea
TiagoRochaMartins <tiagorochamartins19@gmail.com>
2022-10-18 15:12:47 +0100

    Atualização dos novos ficheiros

---
+++ Threads2/basico.py
@@ -0,0 +1,17 @@
+x=20
+y=30
+
+if x>y:
+    print("x é maior que y\n")
+else:
+    print("x é menor que y\n")
+
+for i in range(0,10):
+    print(i)
+    print(" Olá mundo \n")
+print("fim")
+
+def tabua(x):
+    print("tabuada" +str(x))
+
+tabua(2)
